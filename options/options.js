13c7670309525cb321bb1e132cc376133fa1fef3
Rodrigo <rodrigo@crowdbotics.com>
2022-08-16 20:12:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "plat8009_l2_15",
+  url: "https://plat8009_l2_15.botics.co",
+  api: "https://plat8009_l2_15.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
